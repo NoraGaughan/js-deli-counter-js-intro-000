ae8bc9ec57b1e117d81ab72c65951bf2868c1db6
Nora Gaughan <noragaughan@gmail.com>
2020-07-16 19:46:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ function currentLine(katzDeli) {
   for (let i=0; i < katzDeli.length; i++) {
     line.push (` `+[i+1]+`. `  + katzDeli[i]) }
     if (katzDeli.length === 0) { return "The line is currently empty." }
-    else return "The line is currently "
+    else return ("The line is currently:" + line); 
   }
