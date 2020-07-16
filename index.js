eef2b9d8b45e14f855c23ed39a548ab483cdd345
Nora Gaughan <noragaughan@gmail.com>
2020-07-16 19:40:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 function nowServing(katzDeli) {
   let i = 0; 
   while (i < katzDeli.length) { i++; }
- if (katzDeli.length === 0)  
+ if (katzDeli.length === 0) { return "There is nobody waiting to be served!" } 
 }
