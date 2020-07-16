2dbfb4f67f544c77401a076fa6790a3eee66fece
Nora Gaughan <noragaughan@gmail.com>
2020-07-16 19:49:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 var katzDeli = []
 
-function takeANumber(katzDeli, name) { katzDeli.push (`${name}`); 
-return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
-} 
+function takeANumber(katzDeli, name) {
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+
 
 function nowServing(katzDeli) {
-  let i = 0; 
-  while (i < katzDeli.length) { i++; }
- if (katzDeli.length === 0) { return "There is nobody waiting to be served!"; } 
- else return (`Curently serving ${katzDeli.shift()}.`); 
+  let i = 0;
+  while (i < katzDeli.length) {
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}.`);
 }
 
 var line = [];
